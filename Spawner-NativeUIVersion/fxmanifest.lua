7b8793ef73cc10a7356106333fad08907e9f669e
CxldBreeze <cb443151@gmail.com>
2024-08-19 18:18:40 -0500

    hi

---
+++ Spawner-NativeUIVersion/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Breezeified <breeze@breezeified.xyz>'
+description 'A simple vehicle spawner script for FiveM'
+version '1.0.0' 
+
+clientscripts {
+    '@NativeUI/NativeUI.lua',
+    'main.lua'
+}
---
+++ Spawner-RageUIVersion/fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Breezeified <breeze@breezeified.xyz>'
+description 'A simple vehicle spawner script for FiveM'
+version '1.0.0' 
+
+dependencies {
+    "RageUI",
+}
+
+clientscripts {
+    "@RageUI/src/RMenu.lua",
+    "@RageUI/src/menu/RageUI.lua",
+    "@RageUI/src/menu/Menu.lua",
+    "@RageUI/src/menu/MenuController.lua",
+    "@RageUI/src/components/Audio.lua",
+    "@RageUI/src/components/Rectangle.lua",
+    "@RageUI/src/components/Screen.lua",
+    "@RageUI/src/components/Sprite.lua",
+    "@RageUI/src/components/Text.lua",
+    "@RageUI/src/components/Visual.lua",
+    "@RageUI/src/menu/elements/ItemsBadge.lua",
+    "@RageUI/src/menu/elements/ItemsColour.lua",
+    "@RageUI/src/menu/elements/PanelColour.lua",
+    "@RageUI/src/menu/items/UIButton.lua",
+    "@RageUI/src/menu/items/UICheckBox.lua",
+    "@RageUI/src/menu/items/UIList.lua",
+    "@RageUI/src/menu/items/UIProgress.lua",
+    "@RageUI/src/menu/items/UISlider.lua",
+    "@RageUI/src/menu/items/UISliderHeritage.lua",
+    "@RageUI/src/menu/items/UISliderProgress.lua",
+    "@RageUI/src/menu/panels/UIColourPanel.lua",
+    "@RageUI/src/menu/panels/UIGridPanel.lua",
+    "@RageUI/src/menu/panels/UIGridPanelHorizontal.lua",
+    "@RageUI/src/menu/panels/UIPercentagePanel.lua",
+    "@RageUI/src/menu/panels/UIStatisticsPanel.lua",
+    "@RageUI/src/menu/windows/UIHeritage.lua",
+    'main.lua'
+}
